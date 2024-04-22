02dccfe0228f1901dbdffcca5c6447f29032eb7e
minseokKim6823 <alstjr6823@naver.com>
2024-04-22 18:18:30 +0900

    json 변환

--- src/main/java/capstone/capstone/gemini/GeminiProVisionController.java
+++ src/main/java/capstone/capstone/gemini/GeminiProVisionController.java
@@ -7,7 +7,10 @@ import com.google.cloud.vertexai.generativeai.GenerativeModel;
 import com.google.cloud.vertexai.generativeai.PartMaker;
 import com.google.cloud.vertexai.generativeai.ResponseHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,28 +24,15 @@ import java.io.IOException;
 public class GeminiProVisionController {
 
     private final GenerativeModel generativeModel;
-/*
-    @PostMapping
-    public String file(@RequestParam("file")MultipartFile file, @RequestParam String question) throws IOException{
-        GenerateContentResponse generateContentResponse =this.generativeModel.generateContent(
-                ContentMaker.fromMultiModalData(
-                        PartMaker.fromMimeTypeAndData(file.getContentType(), file.getBytes()),
-                        question
-                )
-        );
-        return ResponseHandler.getText(generateContentResponse);
-    }
-    사진
- */
 
     @PostMapping
-    public String file(@RequestParam("file")MultipartFile file, @RequestParam String question) throws IOException{
-        GenerateContentResponse generateContentResponse =this.generativeModel.generateContent(
+    public ResponseEntity<String> processRequest(@RequestBody String question) throws IOException {
+        GenerateContentResponse generateContentResponse = this.generativeModel.generateContent(
                 ContentMaker.fromMultiModalData(
-                        PartMaker.fromMimeTypeAndData(file.getContentType(), file.getBytes()),
                         question
                 )
         );
-        return ResponseHandler.getText(generateContentResponse);
+        String responseText = ResponseHandler.getText(generateContentResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(responseText);
     }
 }
