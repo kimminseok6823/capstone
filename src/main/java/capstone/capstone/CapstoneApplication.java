11c5f64557ea9ea73bfa48a2c4d12939ffe6f109
minseokKim6823 <alstjr6823@naver.com>
2024-05-07 16:43:44 +0900

    mariadb

--- src/main/java/capstone/capstone/CapstoneApplication.java
+++ src/main/java/capstone/capstone/CapstoneApplication.java
@@ -3,7 +3,7 @@ package capstone.capstone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 
 
